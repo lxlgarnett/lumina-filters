a955a5cb1eacd3cbb84f449cad62e57d90559e9b
lxlgarnett <lxlgarnett@gmail.com>
2025-12-27 22:13:13 +0900

    Fix: Enable UNPACK_FLIP_Y_WEBGL to correct inverted image rendering

--- main.js
+++ main.js
@@ -210,6 +210,7 @@
   // Create texture
   const texture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
   // Default filtering
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
